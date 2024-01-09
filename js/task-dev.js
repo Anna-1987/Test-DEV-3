8d58150e72ea9a00b417c248fab31d13a6ac25b8
Anna-1987 <mag260910@gmail.com>
2024-01-09 13:40:56 +0200

    test

--- js/task-dev.js
+++ js/task-dev.js
@@ -1,147 +1,91 @@
 const inputEl = document.querySelector('#textinput');
 const outputEl = document.querySelector('#textoutput');
- console.log(outputEl);
-const buttonEl = document.querySelector('button')
+const buttonEl = document.querySelector('button');
 
-// inputEl.addEventListener('input', onInputChange );
 buttonEl.addEventListener('click', onInputChange )
 
-// outputEl.addEventListener('mousemove', function (event) {
-//    // добавляем обработчик события "mousemove"
-//    const x = event.clientX; // получаем координату X мыши
-//    const y = event.clientY; // получаем координату Y мыши
- 
-//    console.log(`Координаты мыши: x=${x}, y=${y}`); // выводим координаты мыши в консоль
-//  });
-
+let selectedLetters = [];
 
 function onInputChange(){
-   arrayOfChar();
+
+let char = inputEl.value;
+outputEl.innerHTML = "";
+ for (let i = 0; i < char.length; i++) {
+    const letter = char[i];
+    const span = document.createElement('span');
+    span.textContent = letter;
+    console.log (letter);
+   //  span.addEventListener('click', onClick);
+    span.addEventListener('mousedown', (event) => handleMouseDown(event, i));
+     outputEl.appendChild(span);
 }
 
-function arrayOfChar(){
- let char = inputEl.value;
- let charArray = Array.from(char);
- console.log(charArray);
+function handleMouseDown(event, index) {
+   const letter = event.target;
 
-let subArrayCount = charArray.length;
-let result = charArray.reduce((acc, item, index)=> {
-  if (acc[index%subArrayCount] == undefined) acc.push([]);
-  acc[index%subArrayCount].push(item);
-  return acc;
-} , [])
+   if (event) {
+       toggleSelection(letter);
+   } else {
+       clearSelection();
+       selectLetter(letter);
+   }
 
-
-console.log(result);
-
-const wordMarkup = result.map((char) => {
-   return `<div class="lineword">${char}</div>`;
-}).join('');
-
- 
- const outputEl = document.querySelector('#textoutput');
- outputEl.insertAdjacentHTML("beforeend", wordMarkup);
+   document.addEventListener('mousemove', (event) => handleMouseMove(event, index));
+   document.addEventListener('mouseup', () => handleMouseUp());
 }
 
-outputEl.addEventListener('click', onClick);
+function handleMouseMove(event, index) {
+   const selectedLettersCopy = [...selectedLetters];
 
-function onClick(event){
-   if(event.target.nodeName !== 'DIV'){
-      return;
-   };
-   event.target.classList.toggle('activChar');
-   // event.target.classList.toggle('block1');
-    console.log(event);
+   selectedLettersCopy.forEach((letter, i) => {
+       const rect = letter.getBoundingClientRect();
+       const x = event.clientX + window.scrollX - rect.width / 2;
+       const y = event.clientY + window.scrollY - rect.height / 2;
 
+       letter.style.position = 'absolute';
+       letter.style.left = `${x}px`;
+       letter.style.top = `${y}px`;
 
-
+       if (i === selectedLettersCopy.length - 1) {
+           letter.dataset.lastIndex = index;
+       }
+   });
 }
 
-function moveChar() {
-   outputEl.addEventListener('mousedown', event => {
-      document.addEventListener('mousemove', listener);
-  });
-  
-  outputEl.addEventListener('mouseup', event => {
-      document.removeEventListener('mousemove', listener);
-  });
+function handleMouseUp() {
+   selectedLetters.forEach((letter, i) => {
+       const lastIndex = letter.dataset.lastIndex;
+       if (lastIndex !== undefined) {
+           const targetLetter = document.getElementById(`textoutput`).childNodes[lastIndex];
+           targetLetter.parentNode.insertBefore(letter, targetLetter);
+           letter.style.position = 'static';
+           letter.removeAttribute('data-last-index');
+       }
+   });
 
+   document.removeEventListener('mousemove', handleMouseMove);
+   document.removeEventListener('mouseup', handleMouseUp);
 }
 
-const listener = function(e) {
- const x = e.pageX  + e.clientX;
- console.log(x);
- e.pageY  + e.clientX;
- };
+function selectLetter(letter) {
+   letter.classList.add('activ');
+   selectedLetters.push(letter);
+}
 
-// const listener = function(event) {
-   // function getCoords(event) {
-   //    let box = event.getBoundingClientRect();
-    
-   //    return {
-   //      top: box.top + window.pageYOffset,
-   //      right: box.right + window.pageXOffset,
-   //      bottom: box.bottom + window.pageYOffset,
-   //      left: box.left + window.pageXOffset
-   //    };
-   //  }
+function toggleSelection(letter) {
+   if (letter.classList.contains('activ')) {
+       letter.classList.remove('activ');
+       selectedLetters = selectedLetters.filter(selectedLetter => selectedLetter !== letter);
+   } else {
+       selectLetter(letter);
+   }
+}
 
-   //  let mouseOffset
-
-   //  outputEl.onmousedown = function(event) {
-   
-   //      const pos = getBoundingClientRect(event.target);
-   //      console.log(pos);
-
-   //      mouseOffset = { 
-   //          x: e.pageX - pos.x, 
-   //          y: e.pageY - pos.y 
-   //      }
-      
-   //  }
-    
-   //  document.onmousemove = function(e) {
-       
-   //      outputEl.style.left = e.pageX - mouseOffset + 'px'
-   //      outputEl.style.top = e.pageY - mouseOffset  + 'px'
-      
-   //  }
-    
-      
-
-   // let shiftX = event.pageX - getCoords(event.target.textContent).left;
-   // let shiftY = event.pageY - getCoords(event.target.textContent).top;
- 
-   // ball.style.zIndex = 1000; // над другими элементами
- 
-   
-   // outputEl.style.left = e.pageX - shiftX + 'px';
-   // outputEl.style.top = e.pageY - shiftY + 'px';
-   // }
-
-//    document.onmousemove = function(e) {
-//      moveAt(e);
-//    };
- 
-//    ball.onmouseup = function() {
-//      document.onmousemove = null;
-//      ball.onmouseup = null;
-//    };
- 
-//  }
- 
-//  ball.ondragstart = function() {
-//    return false;
-//  };
- 
-
-// outputEl.addEventListener('mousedown', e => {
-//     document.addEventListener('mousemove', listener);
-// });
-
-// outputEl.addEventListener('mouseup', e => {
-//     document.removeEventListener('mousemove', listener);
-// });
+function clearSelection() {
+   selectedLetters.forEach(letter => letter.classList.remove('activ'));
+   selectedLetters = [];
+}
+}
 
 
 
